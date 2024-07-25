4d750be62985459ad9e70cd0e6564db714817714
Adam Salem <adamsalem08@outlook.com>
2024-07-25 13:25:11 -0400

    Updated the paths

--- 2072A/static/main.cpp
+++ 2072A/static/main.cpp
@@ -17,9 +17,9 @@ using namespace rd;		// This is definitely bad practice but it saves time.
 // using the namespaces makes the code more readable for the notebook...
 
 // Declare functions for selector...
-void red_Rush();
+void red_left();
 void red_right();
-void blue_Rush();
+void blue_left();
 void blue_right();
 void skills();
 
@@ -38,9 +38,9 @@ void skills();
 Controller master(E_CONTROLLER_MASTER); // Main controller
 
 // Robodash selector initialization...
-Selector selector({{"Red Left", &red_Rush},
+Selector selector({{"Red Left", &red_left},
 				   {"Red Right", &red_right},
-				   {"Blue Left", &blue_Rush},
+				   {"Blue Left", &blue_left},
 				   {"Blue Right", &blue_right},
 				   {"Skills", &skills}});
 
@@ -50,6 +50,8 @@ Console console;
 // Creates the custom pages
 rd_view_t *view = rd_view_create("Info");
 rd_view_t *auto_override = rd_view_create("Override");
+
+// Initializes the paths for LemLib to follow
 
 ASSET(RedAWPRush_1_txt);
 ASSET(RedAWPRush_2_txt);
@@ -64,7 +66,6 @@ ASSET(BlueFullAWP_1_txt);
 ASSET(BlueFullAWP_2_txt);
 
 ASSET(SkillsRunAuto_1_txt);
-ASSET(SkillsRunAuto_1_5_txt);
 ASSET(SkillsRunAuto_2_txt);
 ASSET(SkillsRunAuto_3_txt);
 ASSET(SkillsRunAuto_4_txt);
@@ -75,35 +76,36 @@ ASSET(SkillsRunAuto_8_txt);
 ASSET(SkillsRunAuto_9_txt);
 ASSET(SkillsRunAuto_10_txt);
 
+
+
 // left motor group
-MotorGroup left_motor_group({-1, -2, -3});
+MotorGroup left_motor_group({1, 2, 3});
 // right motor group
 MotorGroup right_motor_group({4, 5, 6});
+// lift motor group
+MotorGroup lift_motor_group({15, 16});
 // Intake
-Motor intake(14);
-
-MotorGroup lift({-20, 11});
+Motor intake(7);
 // Pneumatics
-adi::Pneumatics mogo('a', true);
-adi::Pneumatics floating('h', false);
-adi::Pneumatics claw('g', false); 
-adi::Pneumatics claw_pivot('b', false); 
-
+adi::Pneumatics mogo('a', false);
+adi::Pneumatics floating('b', false);
+adi::Pneumatics claw('c', false);
+adi::Pneumatics fold('d', false);
 
 Drivetrain drivetrain(&left_motor_group,  // left motor group
 					  &right_motor_group, // right motor group
-					  13.5,				  // TRACK WIDTH
+					  10,				  // TRACK WIDTH, NEEDS MEASUREMENT LATER...
 					  Omniwheel::NEW_325, // using new 3.25" omnis
 					  450,				  // 450 rpm
 					  2					  // horizontal drift is 2 (for now)
 );
 Imu imu(10);					 // IMU on port 10
-Rotation lift_encoder(12);
-Rotation horizontal_encoder(13); // tracking wheel on port 12
+Rotation vertical_encoder(11);	 // tracking wheel on port 11
+Rotation horizontal_encoder(12); // tracking wheel on port 12
+TrackingWheel horizontal_tracking_wheel(&horizontal_encoder, Omniwheel::NEW_275, -5.75);
+TrackingWheel vertical_tracking_wheel(&vertical_encoder, Omniwheel::NEW_275, -5.75);
 
-TrackingWheel horizontal_tracking_wheel(&horizontal_encoder, Omniwheel::NEW_275, 2.5);
-
-OdomSensors sensors(nullptr,	// vertical tracking wheel 1, set to null
+OdomSensors sensors(&vertical_tracking_wheel,	// vertical tracking wheel 1, set to null
 					nullptr,					// vertical tracking wheel 2, set to nullptr as we are using IMEs
 					&horizontal_tracking_wheel, // horizontal tracking wheel 1
 					nullptr,					// horizontal tracking wheel 2, set to nullptr as we don't have a second one
@@ -122,16 +124,16 @@ ControllerSettings lateral_controller(10,  // proportional gain (kP)
 									  20   // maximum acceleration (slew)
 );
 
-// angular (turning) PID controller
-ControllerSettings angular_controller(10, // proportional gain (kP)
-                                              0, // integral gain (kI)
-                                              25, // derivative gain (kD)
-                                              3, // anti windup
-                                              1, // small error range, in degrees
-                                              100, // small error range timeout, in milliseconds
-                                              3, // large error range, in degrees
-                                              500, // large error range timeout, in milliseconds
-                                              0 // maximum acceleration (slew)
+// angular (turing) PID controller
+ControllerSettings angular_controller(2,   // proportional gain (kP)
+									  0,   // integral gain (kI)
+									  10,  // derivative gain (kD)
+									  3,   // anti windup
+									  1,   // small error range, in degrees
+									  100, // small error range timeout, in milliseconds
+									  3,   // large error range, in degrees
+									  500, // large error range timeout, in milliseconds
+									  0	   // maximum acceleration (slew)
 );
 
 // Creates the chassis using LemLib, and applies the settings and sensors to it.
@@ -199,20 +201,14 @@ int update_ui()
 	lv_obj_t *drive_temp_label = lv_label_create(rd_view_obj(view));
 	lv_obj_align(drive_temp_label, LV_ALIGN_TOP_LEFT, 5, 25);
 
-		lv_obj_t *intake_temp_label = lv_label_create(rd_view_obj(view));
-	lv_obj_align(intake_temp_label, LV_ALIGN_TOP_LEFT, 5, 40);
-
-		lv_obj_t *lift_temp_label = lv_label_create(rd_view_obj(view));
-	lv_obj_align(lift_temp_label, LV_ALIGN_TOP_LEFT, 5, 55);
-
 	lv_obj_t *lem_label = lv_label_create(rd_view_obj(view));
-	lv_obj_align(lem_label, LV_ALIGN_TOP_LEFT, 5, 65);
+	lv_obj_align(lem_label, LV_ALIGN_TOP_LEFT, 5, 45);
+
+	lv_obj_t *vert_one_label = lv_label_create(rd_view_obj(view));
+	lv_obj_align(vert_one_label, LV_ALIGN_TOP_LEFT, 5, 150);
 
 	lv_obj_t *horizontal_one_label = lv_label_create(rd_view_obj(view));
 	lv_obj_align(horizontal_one_label, LV_ALIGN_TOP_LEFT, 5, 175);
-
-	lv_obj_t *lift_encoder_label = lv_label_create(rd_view_obj(view));
-	lv_obj_align(lift_encoder_label, LV_ALIGN_TOP_LEFT, 5, 200);
 	// Creates the UI elements
 
 	// Update Loop
@@ -221,13 +217,9 @@ int update_ui()
 		// Updates the labels to show the most recent data.
 		lv_label_set_text(imu_heading_label, ("IMU Heading: " + to_string(round(imu.get_heading()))).c_str());
 		lv_label_set_text(drive_temp_label, ("Average drive temp: " + to_string((left_motor_group.get_temperature() + right_motor_group.get_temperature()) / 2)).c_str());
-		
-				lv_label_set_text(intake_temp_label, ("Intake temp: " + to_string(intake.get_temperature(0))).c_str());
-		lv_label_set_text(lift_temp_label, ("Average lift temp: " + to_string((lift.get_temperature(0) + lift.get_temperature(1))/2)).c_str());
 		lv_label_set_text(lem_label, ("X: " + to_string(chassis.getPose().x) + "\nY: " + to_string(chassis.getPose().y) + "\nTheta: " + to_string(chassis.getPose().theta)).c_str());
+		lv_label_set_text(vert_one_label, ("Vertical tracking 1: " + to_string(vertical_tracking_wheel.getDistanceTraveled())).c_str());
 		lv_label_set_text(horizontal_one_label, ("Horizontal tracking 1: " + to_string(horizontal_tracking_wheel.getDistanceTraveled())).c_str());
-				lv_label_set_text(lift_encoder_label, ("Lift encoder ANGLE: " + to_string((lift_encoder.get_position() / 100) * 0.2)).c_str());
-		master.print(0, 0, "Connection:%d ", master.is_connected());
 		delay(100); // Waits 100ms before updating again to make it readable.
 	}
 	// Update Loop
@@ -259,10 +251,7 @@ void check_device_plugged_in(int port, std::string deviceName)
 void initialize()
 {
 	master.clear();
-	mogo.retract();
-		claw.extend();
-	lift_encoder.reset_position();
-	lift_encoder.reset();
+	mogo.retract(); // Make sure mogo mech is retracted on initialization.
 	// Check if all devices are plugged in
 	console.println("Checking device status...");
 	check_device_plugged_in(imu.get_port(), "IMU");
@@ -272,8 +261,9 @@ void initialize()
 	check_device_plugged_in(right_motor_group.get_port(0), "Right Motor Group 1");
 	check_device_plugged_in(right_motor_group.get_port(1), "Right Motor Group 2");
 	check_device_plugged_in(right_motor_group.get_port(2), "Right Motor Group 3");
+	check_device_plugged_in(vertical_encoder.get_port(), "Vertical Encoder");
 	check_device_plugged_in(horizontal_encoder.get_port(), "Horizontal Encoder");
-	chassis.calibrate();
+	
 	// Start the update_ui task
 	Task t(update_ui);
 }
@@ -319,7 +309,7 @@ void red_Rush()
 	chassis.setPose(-49, -37, 90); // TODO: Zach change to the start of this path...
 	console.println("red left is running...");
 
-	lift.move_absolute(100, 200);
+	lift_motor_group.move_absolute(100, 200);
 	chassis.follow(RedAWPRush_1_txt, 100, 7, false, true);
 	chassis.waitUntilDone();
 	mogo.set_value(true);
@@ -334,10 +324,10 @@ void red_Rush()
 	chassis.waitUntil(56);
 	intake.move(0);
 	mogo.set_value(false);
-	lift.move_absolute(150, 300);
+	lift_motor_group.move_absolute(150, 300);
 	chassis.waitUntilDone();
-	claw_pivot.set_value(true);
-	lift.move_absolute(120, 300);
+	fold.set_value(true);
+	lift_motor_group.move_absolute(120, 300);
 	claw.set_value(true);
 
 	chassis.swingToHeading(135, DriveSide::RIGHT, 500, {.minSpeed = 127, .earlyExitRange = 10});
@@ -354,10 +344,10 @@ void red_Rush()
 	intake.move(100);
 	mogo.set_value(true);
 	chassis.moveToPose(-20, -20, 45, 1000);
-	while (lift.get_efficiency() > 10){
-		lift.move(100);
+	while (lift_motor_group.get_efficiency() > 10){
+		lift_motor_group.move(100);
 	}
-	lift.move(20);
+	lift_motor_group.move(20);
 
 
 
@@ -373,7 +363,7 @@ void blue_Rush()
 	chassis.setPose(49, -37, 270); // TODO: Zach change to the start of this path...
 	console.println("Blue Rush is running...");
 
-	lift.move_absolute(100, 200);
+	lift_motor_group.move_absolute(100, 200);
 	chassis.follow(BlueAWPRush_1_txt, 10, 7, false, true);
 	chassis.waitUntilDone();
 	mogo.set_value(true);
@@ -388,10 +378,10 @@ void blue_Rush()
 	chassis.waitUntil(56);
 	intake.move(0);
 	mogo.set_value(false);
-	lift.move_absolute(150, 300);
+	lift_motor_group.move_absolute(150, 300);
 	chassis.waitUntilDone();
-	claw_pivot.set_value(true);
-	lift.move_absolute(120, 300);
+	fold.set_value(true);
+	lift_motor_group.move_absolute(120, 300);
 	claw.set_value(true);
 
 	chassis.swingToHeading(135, DriveSide::RIGHT, 500, {.minSpeed = 127, .earlyExitRange = 10});
@@ -408,10 +398,10 @@ void blue_Rush()
 	intake.move(100);
 	mogo.set_value(true);
 	chassis.moveToPose(20, -20, 315, 1000);
-	while (lift.get_efficiency() > 10){
-		lift.move(100);
+	while (lift_motor_group.get_efficiency() > 10){
+		lift_motor_group.move(100);
 	}
-	lift.move(20);
+	lift_motor_group.move(20);
 }
 void blue_right()
 {
@@ -420,39 +410,36 @@ void skills()
 {
 	chassis.setPose(-55, 0, 270); // TODO: Zach change to the start of this path...
 	console.println("Skills is running...");
-	lift.move(-127);
-	floating.retract();
-	//lift.move_absolute(150, 300);
-	claw_pivot.set_value(true);
-	//lift.move_absolute(120, 300);
-	claw.set_value(true);
-	claw_pivot.set_value(false);
 
-	delay(500);
-	lift.brake();
-	
-	chassis.follow(SkillsRunAuto_1_txt, 7, 10000, false, true);
+	lift_motor_group.move_absolute(150, 300);
+	fold.set_value(true);
+	lift_motor_group.move_absolute(120, 300);
+	claw.set_value(true);
+	fold.set_value(false);
+
+	chassis.follow(SkillsRunAuto_1_txt, 10, 7, false, true);
 	chassis.waitUntilDone();
 
-	mogo.extend();
-	intake.move(-110);
-	chassis.follow(SkillsRunAuto_1_5_txt, 10, 100000, true, true);
+	mogo.set_value(true);
+	intake.move(110);
+	chassis.follow(SkillsRunAuto_2_txt, 10, 7, true, true);
 
-	chassis.follow(SkillsRunAuto_2_txt, 10, 100000, true, true);
-
-	chassis.waitUntil(70);
-	mogo.retract();
-	//lift.move_absolute(0, 300);
-	claw_pivot.set_value(true);
+	chassis.waitUntil(175);
+	mogo.set_value(false);
+	lift_motor_group.move_absolute(0, 300);
+	fold.set_value(true);
 	chassis.waitUntilDone();
 	claw.set_value(false);
-	//lift.move_absolute(150, 300);
+	lift_motor_group.move_absolute(150, 300);
 	chassis.moveToPoint(0, 50, 500);
-	//lift.move_absolute(120, 300);
+	lift_motor_group.move_absolute(120, 300);
 	claw.set_value(true);
-	claw_pivot.set_value(false);
-}
+	fold.set_value(false);
 
+
+
+
+}
 
 /**
  * Runs the operator control code. This function will be started in its own task
@@ -470,51 +457,37 @@ void skills()
 void opcontrol()
 {
 	// Loop indefinitely
+	bool mogo_tog = true;
+	bool claw_tog = true;
+	bool fold_tog = true;
+	bool floating_tog = true;
+
+	bool mogo_tog_check = false;
+	bool claw_tog_check = false;
+	bool fold_tog_check = false;
+	bool floating_tog_check = false;
+
 	while (true)
 	{
-		intake.set_brake_mode(E_MOTOR_BRAKE_COAST);
-		lift.set_brake_mode_all(E_MOTOR_BRAKE_HOLD);
-		
 		// Get the left and right y-axis positions on the controller
 		int leftY = master.get_analog(E_CONTROLLER_ANALOG_LEFT_Y);
 		int rightY = master.get_analog(E_CONTROLLER_ANALOG_RIGHT_Y);
 
 		// Move the robot based on the controller inputs
 		chassis.tank(leftY, rightY);
-		//chassis.arcade(leftY, master.get_analog(E_CONTROLLER_ANALOG_RIGHT_X));
 
-		// Toggle mogo mech
-		if(master.get_digital_new_press(E_CONTROLLER_DIGITAL_R1))
-			mogo.toggle();
-		if(master.get_digital_new_press(E_CONTROLLER_DIGITAL_UP))
-			claw.toggle();
-		
-		if (master.get_digital(E_CONTROLLER_DIGITAL_R2)){
-			intake.move(-127);
-		}
-		else if (master.get_digital(E_CONTROLLER_DIGITAL_DOWN))
-			intake.move(127);
-		else
-		 	intake.move(0);
-		
-		if (master.get_digital(E_CONTROLLER_DIGITAL_L2)){
-			lift.move(127);
-		}
-		else if (master.get_digital(E_CONTROLLER_DIGITAL_L1))
-			lift.move(-127);
-		else{
-		 	lift.brake();
+		// Pneumatic Toggles
+		buttonToggle(master.get_digital(E_CONTROLLER_DIGITAL_L1), mogo, mogo_tog, mogo_tog_check);
+		buttonToggle(master.get_digital(E_CONTROLLER_DIGITAL_L2), claw, claw_tog, claw_tog_check);
+		buttonToggle(master.get_digital(E_CONTROLLER_DIGITAL_Y), fold, fold_tog, fold_tog_check);
+		buttonToggle(master.get_digital(E_CONTROLLER_DIGITAL_RIGHT), floating, floating_tog, floating_tog_check);
 
-			
-		}
+		if (master.get_digital(E_CONTROLLER_DIGITAL_R1)) {
+			lift_motor_group.move(110);
+		} else if (master.get_digital(E_CONTROLLER_DIGITAL_R1)) {
+			lift_motor_group.move(-110);
+		} else {lift_motor_group.move(0);}
 
-		if(master.get_digital_new_press(E_CONTROLLER_DIGITAL_LEFT))
-			skills();
-
-
-		//if ((lift_encoder.get_position() / 100 * 0.2) > 15)
-		//	master.rumble("-");
-		
 		// Delay to save resources
 		delay(25);
 	}
